755847334df86cf1e2f0ddede52662f8fc84295e
SM2116 <SM2116@DESKTOP-2O6A12E>
2021-06-15 18:25:24 +0900

    나의 첫 깃

---
+++ 1stWeek/src/day01/ex01변수.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class ex01변수 {
+
+	public static void main(String[] args) {
+		// 변수 생성하기
+		int age = 10;
+		String name = "가나다";
+		char name2 = '가';
+		
+		//숫자를 문자로 형변환
+		int age2=10;
+		String age3=String.valueOf(age2);
+		
+		//문자를 숫자로 형변환
+		String name3="50";
+		
+		//int형 데이터 float형 데이터 형변환 정수 -> 실수
+		int number=3;
+		float number2=3.0f;
+		float number3=(float)3.5f;
+		int number4=(int)number2;
+		
+		
+	}
+
+}
